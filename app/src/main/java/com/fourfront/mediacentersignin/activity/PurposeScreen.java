445d70246fd5b9e19341cc97e2609fdefdff476b
Kevin Chen <kchen1250@gmail.com>
2017-05-24 23:53:16 -0400

    Add button capability for purpose page

--- app/src/main/java/com/fourfront/mediacentersignin/activity/PurposeScreen.java
+++ app/src/main/java/com/fourfront/mediacentersignin/activity/PurposeScreen.java
@@ -1,31 +1,69 @@
 package com.fourfront.mediacentersignin.activity;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.fourfront.mediacentersignin.R;
 import com.fourfront.mediacentersignin.helper.Student;
 
+import static com.fourfront.mediacentersignin.R.id.next;
+
 public class PurposeScreen extends AppCompatActivity {
 
     private Student student;
+    private RadioGroup rGroup;
+    private Button finish;
+
+    private RadioGroup.OnCheckedChangeListener m = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+            finish.setEnabled(true);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purpose_screen);
 
+        Intent intent = getIntent();
+        student = (Student) intent.getSerializableExtra("STUDENT");
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
-        Intent intent = getIntent();
-        student = (Student) intent.getSerializableExtra("STUDENT");
+        finish = (Button) findViewById(R.id.finish);
+        rGroup = (RadioGroup) findViewById(R.id.listOfReasons);
+        rGroup.setOnCheckedChangeListener(m);
+
+        addRadioButton("Reason 1",  1);
+        addRadioButton("Reason 2",  2);
+        addRadioButton("Reason 3",  3);
+        addRadioButton("Reason 4",  4);
+        addRadioButton("Reason 5",  5);
+        addRadioButton("Reason 6",  6);
+        addRadioButton("Reason 7",  7);
+        addRadioButton("Reason 8",  8);
+        addRadioButton("Reason 9",  9);
+        addRadioButton("Reason 10",  10);
+        addRadioButton("Reason 11",  11);
+        addRadioButton("Reason 12",  12);
+        addRadioButton("Reason 13",  13);
+        addRadioButton("Reason 14",  14);
+        addRadioButton("Reason 15",  15);
     }
 
     @Override
@@ -36,5 +74,24 @@ public class PurposeScreen extends AppCompatActivity {
             startActivity(intent);
         }
         return true;
+    }
+
+    private void addRadioButton(String str, int id) {
+        RadioButton rb = new RadioButton(PurposeScreen.this);
+        rb.setText(str);
+        rb.setId(id);
+        rb.setGravity(Gravity.TOP);
+        rb.setPadding(10, 0, 0, 10);
+        rb.setTextSize(22);
+        rb.setTextColor(getResources().getColorStateList(R.color.radio_button_style));
+        rGroup.addView(rb);
+    }
+
+    public void sendInfoDone(View view) {
+        Toast.makeText(PurposeScreen.this, "Thank you for signing in.", Toast.LENGTH_SHORT).show();
+        student.saveToFile("Teacher", "No", "Test reason");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/fourfront/mediacentersignin/activity/SelectTeacher.java
+++ app/src/main/java/com/fourfront/mediacentersignin/activity/SelectTeacher.java
@@ -1,8 +1,7 @@
 package com.fourfront.mediacentersignin.activity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.provider.CalendarContract;
+import android.os.Environment;
 import android.support.annotation.IdRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.fourfront.mediacentersignin.R;
 import com.fourfront.mediacentersignin.helper.Student;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SelectTeacher extends AppCompatActivity {
@@ -32,7 +33,7 @@ public class SelectTeacher extends AppCompatActivity {
     private String selectedFirst;
     private String selectedLast;
 
-    private OnCheckedChangeListener m = new OnCheckedChangeListener() {
+    private RadioGroup.OnCheckedChangeListener m = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
             next.setEnabled(true);
@@ -52,8 +53,6 @@ public class SelectTeacher extends AppCompatActivity {
         Intent intent = getIntent();
         student = (Student) intent.getSerializableExtra("STUDENT");
 
-        student.saveToFile("Teacher", "No", "Test reason");
-
         name = (TextView) findViewById(R.id.nameText);
         next = (Button) findViewById(R.id.next);
 
@@ -69,12 +68,30 @@ public class SelectTeacher extends AppCompatActivity {
         ArrayList<String> teachers = student.getTeacherNames();
         ArrayList<String> courses = student.getCourses();
         ArrayList<String> semesters = student.getDurations();
+        ArrayList<String> extraStaff = new ArrayList<>();
         String counselor = student.getCounselorName();
 
+        int id = 1;
         for (int i = 0; i < teachers.size(); i++) {
-            addRadioButton(teachers.get(i) + "\n" + courses.get(i).toUpperCase(), i + 1);
+            addRadioButton(teachers.get(i) + "\n" + courses.get(i).toUpperCase(), id++);
         }
-        addRadioButton(counselor + "\nCOUNSELING", teachers.size() + 2);
+        addRadioButton(counselor + "\nCOUNSELING", id++);
+
+        String path = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DOCUMENTS + "/MediaCenterSignIn/extra_staff.txt";
+        try (FileReader fr = new FileReader(path);
+             BufferedReader br = new BufferedReader(fr)) {
+            for (String line = null; (line = br.readLine()) != null;) {
+                extraStaff.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] split;
+        for (String i: extraStaff) {
+            split = i.substring(1, i.length() - 1).split("\",\"", -1);
+            addRadioButton(split[0] + "\n" + split[1].toUpperCase(), id++);
+        }
     }
 
     private void addRadioButton(String str, int id) {
@@ -83,7 +100,7 @@ public class SelectTeacher extends AppCompatActivity {
         rb.setId(id);
         rb.setGravity(Gravity.TOP);
         rb.setPadding(10, 0, 0, 10);
-        rb.setTextSize(20);
+        rb.setTextSize(22);
         rb.setTextColor(getResources().getColorStateList(R.color.radio_button_style));
         rGroup.addView(rb);
     }
